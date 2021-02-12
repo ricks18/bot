807ad7adf2437a76ac21efb0331bd7c5cbf65d89
ricks18 <77172928+ricks18@users.noreply.github.com>
2021-02-12 01:40:06 -0300

    Create sound.js

---
+++ database/menu/sound.js
@@ -0,0 +1,35 @@
+const sound = (prefix, botName, ownerName) => {
+        return `
+┏ *〈 ${botName} 〉*
+╿
+┷┯ *〈 BOT INFO 〉*
+   ╽
+   ┠≽ *Prefix* : 「  ${prefix}  」
+   ┠≽ *Creator* : ${ownerName}
+   ┠≽ *Version* : 0.0.5
+   ╿
+┯┷ *〈 ABOUT 〉*
+╽
+┠≽ *${prefix}info* (error)
+┃ *Desc* : Show Bot Details
+┠──────────────╼
+┠≽ *${prefix}blocklist*
+┃ *Desc* : Show Blocked User
+┠──────────────╼
+┠≽ *${prefix}chatlist* (error)
+┃ *Desc* : Show All Chat User
+┠──────────────╼
+┠≽ *${prefix}ping*
+┃ *Desc* : Show Connection Bot Speed
+┠──────────────╼
+┠≽ *${prefix}bugreport* <text>
+┃ *Desc* : Report Bug To Owner Bot
+╿
+┷┯ *〈 TEXT TO SPEACH 〉*
+   ╽
+   ┠≽ *${prefix}tts* <código de idioma | texto>
+   ┃ *Desc* : Texto para voz do Google
+   ╿ *${ownerName}*,
+   ╰╼≽ *Developer © ${botName}`
+}
+exports.sound = sound
